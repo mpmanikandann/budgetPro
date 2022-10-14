d0820fe6aa436add38002a9c587a6c6b56fab983
manikandan <manikandan.p@kudelskisecurity.com>
2022-10-14 13:41:15 +0530

    Inital Setup

---
+++ src/reducers/budget.js
@@ -0,0 +1,12 @@
+import { ADD_BUDGET, LOAD_BUDGET } from "../utils/actionTypes";
+
+export default (state = [], action) => {
+  switch (action.type) {
+    case LOAD_BUDGET:
+      return action.payload.data;
+    case ADD_BUDGET:
+      return [...state, action.payload.data];
+    default:
+      return state;
+  }
+};
---
+++ src/utils/actionTypes.js
@@ -0,0 +1,4 @@
+// Budget releated action types;
+export const LOAD_BUDGET = "LOAD_BUDGETS";
+export const ADD_BUDGET = "ADD_BUDGET";
+export const DELETE_BUDGET = "DELETE_BUDGET";
